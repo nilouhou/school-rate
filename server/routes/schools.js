1d52c6acabec2b5e48a0a76b818c139a4efd2ff4
Nilou <nilou.houshmand@gmail.com>
2022-01-13 09:00:37 -0800

    created routes file to read schools  list and school details from json file

--- server/routes/schools.js
+++ server/routes/schools.js
@@ -1,6 +1,24 @@
 const { Router } = require("express");
-const { readFile, writeFile } = require("../utils/helpers");
+const { readFile, writeFile } = require("../utils/helper");
 const fs = require("fs");
 
 const schoolRouter = Router();
 const FILE_PATH = "./data/schools.json";
+
+//Get all schools
+schoolRouter.get("/", (req, res) => {
+	let schoolsData = readFile(FILE_PATH);
+
+	return res.status(200).send(schoolsData);
+});
+
+//Get a single school by id
+schoolRouter.get("/:id", (req, res) => {
+	const schools = JSON.parse(fs.readFileSync(FILE_PATH));
+
+	const school = schools.find((school) => school.id === req.params.id);
+	if (!school) return res.status(404).send("The school is not found");
+	res.status(200).json(school);
+});
+
+module.exports = schoolRouter;
--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ const schoolRouter = require("./routes/schools");
 
 const app = express();
 
-const { PORT, CLIENT_URL } = process.env;
+const PORT = process.env.PORT || 5050;
 
-app.use(cors({ origin: CLIENT_URL }));
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+	})
+);
 app.use(express.json());
 app.use("/schools", schoolRouter);
 
