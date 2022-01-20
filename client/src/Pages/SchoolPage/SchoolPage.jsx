e26987e89994771fd55dd253b220b128b7742a8f
Nilou <nilou.houshmand@gmail.com>
2022-01-19 16:46:08 -0800

    added reading rate and user rating UI

--- client/src/Pages/SchoolPage/SchoolPage.jsx
+++ client/src/Pages/SchoolPage/SchoolPage.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./SchoolPage.scss";
 import { getData } from "../../utils";
 
+import Rating from "@mui/material/Rating";
+
 const SchoolPage = (props) => {
-	console.log({ props });
+	const [userRate, setUserRate] = useState(0);
 	const [school, setSchool] = useState([]);
 	useEffect(() => {
 		const { id } = props.match.params;
@@ -13,12 +15,29 @@ const SchoolPage = (props) => {
 	}, []);
 
 	console.log({ school });
+	const { address, area, category, name, rate, rank, recordid } = school;
 
 	return (
 		<div className="school">
 			<div className="school__banner"></div>
 			<div className="school__name">
-				<h1>{school.name}</h1>
+				<h1>{name}</h1>
+			</div>
+			{/* <Comments userId="1" /> */}
+
+			<div className="container">
+				Rate from API {rate}
+				<Rating name="read-only" value={rate ? rate : 0} readOnly />
+				<div className="review">
+					user rate
+					<Rating
+						name="simple-controlled"
+						value={userRate}
+						onChange={(event, newValue) => {
+							setUserRate(newValue);
+						}}
+					/>
+				</div>
 			</div>
 		</div>
 	);
